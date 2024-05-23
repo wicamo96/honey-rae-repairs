9f91f469b307a3a9d6f2c6cdd58155973e9b84cc
Will Moran <will.c.moran@gmail.com>
2024-05-23 13:24:54 -0400

    Added scripts for navbar and routes to handle rendering customer list, employee list, and ticket list

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
 import "./App.css"
 import { CustomerList } from "./components/customers/CustomersList.jsx"
+import { NavBar } from "./components/nav/NavBar.jsx"
 import { StaffList } from "./components/staff/StaffList.jsx"
 import { TicketList } from "./components/tickets/TicketList.jsx"
+import { Routes, Route, Outlet } from "react-router-dom"
 
 export const App = () => {
-  return <>
-    {/* <TicketList /> */}
-    {/* <CustomerList /> */}
-    <StaffList />
-  </>
+  return (
+    <Routes>
+      <Route
+        path="/" 
+        element={
+          <>
+            <NavBar />
+            <Outlet />
+          </>
+        }
+      >
+        <Route path="tickets" element={<TicketList />} />
+        <Route path="customers" element={<CustomerList />} />
+      </Route>
+    </Routes>
+  )
 }
