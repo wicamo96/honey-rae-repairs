70022c66a436582d728436b25879d4e87f5f7bad
Will Moran <will.c.moran@gmail.com>
2024-05-29 09:09:21 -0400

    Back on track

--- src/views/ApplicationViews.jsx
+++ src/views/ApplicationViews.jsx
@@ -1,13 +1,4 @@
-import { Route, Outlet, Routes } from "react-router-dom"
-import { NavBar } from "../components/nav/NavBar.jsx"
-import { Welcome } from "../components/welcome/Welcome.jsx"
-import { StaffDetails } from "../components/staff/StaffDetails.jsx"
-import { StaffList } from "../components/staff/StaffList.jsx"
-import { TicketList } from "../components/tickets/TicketList.jsx"
-import { CustomerList } from "../components/customers/CustomersList.jsx"
-import { CustomerDetails } from "../components/customers/CustomerDetails.jsx"
 import { useEffect, useState } from "react"
-import { EmployeeForm } from "../components/forms/EmployeeForms.jsx"
 import { EmployeeViews } from "./EmployeeViews.jsx"
 import { CustomerViews } from "./CustomerViews.jsx"
 
